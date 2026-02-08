570bb459c9be973fff1a4c66ffdd6b88b09e6e99
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-08 16:00:37 -0500

    completed second layout

---
+++ Block Layout/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  Block Layout
+//
+//  Created by ☆ on 08/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
