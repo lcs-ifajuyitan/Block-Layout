1d84863da3bb9494af0d4115b613dcc255541b51
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-08 15:24:34 -0500

    Initial Commit

---
+++ Block Layout/Block_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_LayoutApp.swift
+//  Block Layout
+//
+//  Created by ☆ on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Block_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
