c4d2a3709f1bc8db93af476d663ffbedde6b7c5e
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-08 17:15:53 -0500

    block 4 completed

---
+++ Block Layout/ContentView5.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView5.swift
+//  Block Layout
+//
+//  Created by ☆ on 08/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView5()
+}
