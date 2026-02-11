d2b93ad5689bfc7441fae66164a2c43dc35e22f5
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-11 09:23:48 -0500

    first e.g. of block with text

---
+++ Block Layout/BlockWText.swift
@@ -0,0 +1,18 @@
+//
+//  BlockWText.swift
+//  Block Layout
+//
+//  Created by ☆ on 09/02/2026.
+//
+
+import SwiftUI
+
+struct BlockWText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlockWText()
+}
---
+++ Block Layout/BlockWText2.swift
@@ -0,0 +1,18 @@
+//
+//  BlockWText2.swift
+//  Block Layout
+//
+//  Created by ☆ on 11/02/2026.
+//
+
+import SwiftUI
+
+struct BlockWText2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlockWText2()
+}
